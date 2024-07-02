46f9e1752be47811b63745a7bcdbdce5968c5284
CMA2401PT <1634268014@qq.com>
2024-07-03 01:59:17 +0800

    match package version and add a simple example

---
+++ component1/main.go
@@ -0,0 +1,127 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"strings"
+
+	neomega_backbone "github.com/OmineDev/neomega-backbone"
+	"github.com/OmineDev/neomega-core/neomega"
+)
+
+// 对于 debug 框架，使用 go build -buildmode=plugin -o dynamic_component1.so main.go 编译
+// 对于 release 框架(此处存疑)，使用 go build -buildmode=plugin -trimpath -o dynamic_component1.so main.go
+
+// Exported 供 omega 调用以创建一个插件实例
+// 因为一份插件可能对应多个配置文件，所以每个配置都需要创建一个实例
+func Exported() neomega_backbone.DynamicComponent {
+	return &MyDynamicComponent{}
+}
+
+func init() {
+	fmt.Println("dynamic component 1 程序成功被读取")
+}
+
+type MyDynamicComponentConfig struct {
+	Vesion              string   `json:"Version"`
+	Suffix              string   `json:"口癖"`
+	Repeator            string   `json:"复读机"`
+	Hint                string   `json:"请求输入提示"`
+	TerminalTrigger     []string `json:"终端触发词"`
+	GameTrigger         []string `json:"游戏内触发词"`
+	DescripetionOnStart string   `json:"启动时提示"`
+}
+
+type MyDynamicComponent struct {
+	neomega_backbone.BasicDynamicComponent
+	config MyDynamicComponentConfig
+}
+
+func (c *MyDynamicComponent) Init(
+	cfg neomega_backbone.DynamicComponentConfig,
+	storage neomega_backbone.StorageAndLogAccess,
+) {
+	bs, err := json.Marshal(cfg.Configs())
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Dynamic Component 1 原始配置: %v\n", string(bs))
+	json.Unmarshal(bs, &c.config)
+	if c.config.Vesion == "" { // 假如这个配置还没初始化
+		c.config = MyDynamicComponentConfig{
+			Vesion:          "0.0.1",
+			Suffix:          "喵喵~ ",
+			Repeator:        "2401PT",
+			Hint:            "喵? (随便输入点什么)",
+			TerminalTrigger: []string{"r", "echo", "repeat"},
+			GameTrigger:     []string{"r", "echo", "repeat"},
+		}
+		cfg.Upgrade(c.config) // 更新配置文件
+	}
+	if c.config.Vesion == "0.0.1" { // 假如这个配置是老版本的
+		c.config.Vesion = "0.0.2"
+		c.config.DescripetionOnStart = "扣%v和复读机交互"
+		cfg.Upgrade(c.config) // 更新配置文件
+	}
+	fmt.Println("Dynamic Component 1 Init!")
+}
+
+func (c *MyDynamicComponent) doGameEcho(pk neomega.PlayerKit, msg string) {
+	pk.Say(fmt.Sprintf("%v: %v%v", c.config.Repeator, msg, c.config.Suffix))
+}
+
+func (c *MyDynamicComponent) onGameMenu(chat *neomega.GameChat) {
+	pk, found := c.Frame.GetPlayerInteract().GetPlayerKit(chat.Name)
+	if !found {
+		return
+	}
+	if len(chat.Msg) > 0 {
+		c.doGameEcho(pk, chat.Msg[0])
+	} else {
+		pk.Say(c.config.Hint)
+		pk.InterceptJustNextInput(func(chat *neomega.GameChat) {
+			c.doGameEcho(pk, chat.Msg[0])
+		})
+	}
+}
+
+func (c *MyDynamicComponent) doTerminalEcho(msg string) {
+	c.Frame.Logger().Write(fmt.Sprintf("%v: %v%v", c.config.Repeator, msg, c.config.Suffix))
+}
+
+func (c *MyDynamicComponent) onTerminalMenu(chat []string) {
+	if len(chat) > 0 {
+		c.doTerminalEcho(chat[0])
+	} else {
+		fmt.Println(c.config.Hint)
+		c.Frame.GetTerminalInput().AsyncCallBackInGoRoutine(func(nextInput string) {
+			c.doTerminalEcho(strings.TrimSpace(nextInput))
+		})
+	}
+}
+
+func (c *MyDynamicComponent) Inject(frame neomega_backbone.ExtendOmega) {
+	c.Frame = frame
+	fmt.Println("Dynamic Component 1 Injected!")
+	frame.AddGameMenuEntry(&neomega_backbone.GameMenuEntry{
+		MenuEntry: neomega_backbone.MenuEntry{
+			Triggers:     c.config.GameTrigger,
+			ArgumentHint: "[要复读的内容]",
+			Usage:        "和复读机互动",
+		},
+		OnTrigCallBack: c.onGameMenu,
+	})
+	frame.AddBackendMenuEntry(&neomega_backbone.BackendMenuEntry{
+		MenuEntry: neomega_backbone.MenuEntry{
+			Triggers:     c.config.TerminalTrigger,
+			ArgumentHint: "[要复读的内容]",
+			Usage:        "和复读机互动",
+		},
+		OnTrigCallBack: c.onTerminalMenu,
+	})
+}
+
+func (c *MyDynamicComponent) Activate() {
+	fmt.Println("Dynamic Component 1 Activate!")
+	fmt.Println(fmt.Printf(c.config.DescripetionOnStart, c.config.TerminalTrigger[0]))
+}
---
+++ go.mod
@@ -0,0 +1,44 @@
+module dynamic_components
+
+go 1.20
+
+require (
+	github.com/OmineDev/neomega-core v0.0.1
+	github.com/gorilla/websocket v1.5.0
+	github.com/mitchellh/mapstructure v1.5.0
+	github.com/pterm/pterm v0.12.29
+	golang.org/x/term v0.17.0
+	gopkg.in/yaml.v2 v2.4.0
+)
+
+exclude github.com/pterm/pterm v0.12.40
+
+exclude github.com/pterm/pterm v0.12.65
+
+require (
+	github.com/MarvinJWendt/testza v0.2.2 // indirect
+	github.com/OmineDev/neomega-backbone v0.0.2 // indirect
+	github.com/OmineDev/qq-bot-helper v0.0.1 // indirect
+	github.com/andybalholm/brotli v1.0.3 // indirect
+	github.com/atomicgo/cursor v0.0.1 // indirect
+	github.com/df-mc/atomic v1.10.0 // indirect
+	github.com/go-gl/mathgl v1.1.0 // indirect
+	github.com/golang/snappy v0.0.4 // indirect
+	github.com/google/uuid v1.5.0 // indirect
+	github.com/gookit/color v1.5.3 // indirect
+	github.com/klauspost/compress v1.13.6 // indirect
+	github.com/kr/pretty v0.1.0 // indirect
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
+	github.com/mattn/go-runewidth v0.0.15 // indirect
+	github.com/muhammadmuzzammil1998/jsonc v1.0.0 // indirect
+	github.com/rivo/uniseg v0.4.4 // indirect
+	github.com/sandertv/go-raknet v1.12.0 // indirect
+	github.com/ugorji/go/codec v1.2.12 // indirect
+	github.com/xo/terminfo v0.0.0-20220910002029-abceb7e1c41e // indirect
+	golang.org/x/crypto v0.19.0 // indirect
+	golang.org/x/image v0.0.0-20191009234506-e7c1f5e7dbb8 // indirect
+	golang.org/x/sys v0.17.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
+	gopkg.in/square/go-jose.v2 v2.6.0 // indirect
+)
